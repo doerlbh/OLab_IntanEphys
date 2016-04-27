4a486a9441e3ddbd69d7e431f8c33bdecd3458e5
doerlbh <doerlbh@gmail.com>
2016-04-27 05:21:53 -0700

    add figure output options

--- test.m
+++ test.m
@@ -1,5 +1,6 @@
 prompt = 'What is your folder?: ';
 path = input(prompt,'s');
+% /Users/DoerLBH/Dropbox/git/OLab_IntanEphys/Data/test
 
 % So that I can specify a folder to access all data files.
 
@@ -17,47 +18,3 @@ trials = unique(trials);
 trials = trials(~cellfun('isempty',trials));
 
 index = strfind(files, trials{1});
-
-for trial = 1 : length(trials)
-    index = strfind(files, trials{trial});
-    indexmat = cell2mat(index);
-    [~,first,~] = unique(indexmat, 'first');
-    arrange_Intan_RHD(files{first(trial)});
-    
-    amp_data = amplifier_data;
-    ai_data = aux_input_data;
-    bdi_data = board_dig_in_data;
-    sv_data = supply_voltage_data;
-    t_amp = t_amplifier;
-    t_ai = t_aux_input;
-    t_d = t_dig;
-    t_sv = t_supply_voltage;
-    
-    if length(index) > 1
-        for ind = 2 : length(index)
-            if index{ind} == 1
-                arrange_Intan_RHD(files{ind});
-                amp_data = [amp_data, amplifier_data];
-                ai_data = [ai_data, aux_input_data];
-                bdi_data = [bdi_data, board_dig_in_data];
-                sv_data = [sv_data, supply_voltage_data];
-                t_amp = [t_amp, t_amplifier];
-                t_ai =  [t_ai,t_aux_input];
-                t_d = [t_d, t_dig];
-                t_sv = [t_sv, t_supply_voltage];
-            end
-        end
-    end
-    
-    amplifier_data = amp_data;
-    aux_input_data = ai_data;
-    board_dig_in_data = bdi_data;
-    supply_voltage_data = sv_data;
-    t_amplifier = t_amp;
-    t_aux_input = t_ai;
-    t_dig = t_d;
-    t_supply_voltage = t_sv;
-    
-    disp('----------------')
-end
-
